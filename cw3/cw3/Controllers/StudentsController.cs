d49631500a1db759a2b7d65d9926e87129876ef1
jzaczek1 <s18593@pjwstk.edu.pl>
2020-03-17 20:28:02 +0100

    final

--- cw3/cw3/Controllers/StudentsController.cs
+++ cw3/cw3/Controllers/StudentsController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using cw3.DAL;
+using cw3.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace cw3.Controllers
 {
@@ -11,14 +10,23 @@ namespace cw3.Controllers
     public class StudentsController : ControllerBase
     {
 
-        //zad3
-        [HttpGet]
-        public string getStudents()
-        {
-            return "Jan Żaczek";
-        }
+        //zad8====================================================================
+        private readonly IDbService _dbservice;
+        public StudentsController(IDbService dbService) => _dbservice = dbService;
 
-        //zad4
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbservice.GetStudents());
+        }
+        //zad3=================================================================
+        //[HttpGet]
+        //public string getStudents()
+        //{
+        //    return "Jan Żaczek";
+        //}
+
+        ////zad4================================================================
         [HttpGet("{id}")]
         public IActionResult getStudents(int id)
         {
@@ -26,12 +34,40 @@ namespace cw3.Controllers
             {
                 return Ok("Kowalski");
             }
-            else if(id == 2)
+            else if (id == 2)
             {
                 return Ok("Nowak");
             }
             return NotFound("Nie znaleziono studenta");
         }
+
+        //zad5===================================================================
+        //[HttpGet]
+        //public string getStudents(string orderBy)
+        //{
+        //    return $"To jest string z orderBy, sortowanie = {orderBy}";
+        //}
+
+        //zad6===================================================================
+        [HttpPost]
+        public IActionResult createStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+        //zad7===================================================================
+        [HttpPut("{id}")]
+        public IActionResult changeStudent(int id)
+        {
+            return Ok("Aktualizacja dokończona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult deleteStudent(int id)
+        {
+            return Ok("Usuwanie zakończone");
+        }
+
         //       [HttpGet("getStudents")]    // jezeli dam tutaj w nawiasie stringa to jest kolejny po slashu czesc w URL
         // httpget zeby wyciagnac dane o studencie
         //[HttpGet("{id}")] - jezeli damy zadanie daj studenta z id 2 w url to "pod spodem" w tym kodzie wyswietli nam studenta z id =2
