efb16cf6f990b41980d6349d77dd03a49c2878a7
jzaczek1 <s18593@pjwstk.edu.pl>
2020-03-17 19:32:48 +0100

    backup

---
+++ cw3/cw3/Controllers/StudentsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+
+        //zad3
+        [HttpGet]
+        public string getStudents()
+        {
+            return "Jan Żaczek";
+        }
+
+        //zad4
+        [HttpGet("{id}")]
+        public IActionResult getStudents(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if(id == 2)
+            {
+                return Ok("Nowak");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+        //       [HttpGet("getStudents")]    // jezeli dam tutaj w nawiasie stringa to jest kolejny po slashu czesc w URL
+        // httpget zeby wyciagnac dane o studencie
+        //[HttpGet("{id}")] - jezeli damy zadanie daj studenta z id 2 w url to "pod spodem" w tym kodzie wyswietli nam studenta z id =2
+        //lecz wtedy musimy dac query paramater int id - takie samo
+
+        //[HttpGet("{id}")]
+        //public string getStudents(int id)
+        //{
+        //    if (id == 1)
+        //    {
+        //        return "Kowalski";
+        //    }
+        //    else if (id == 2)
+        //    {
+        //        return "Malewski";
+        //    }
+
+        //    return ("Nie znaleziono studenta");
+        //}
+
+        //[HttpGet]
+        //public string getStudents(string orderby)
+        //{
+        //    return $"sortowanie={orderby}";
+        //}
+
+    }
+}
